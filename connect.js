9ac9a2a8ed7b2b4a61edc89841e04607eb7925b8
asutosh-das <ashutoshdas788@gmail.com>
2026-03-12 21:13:09 +0530

    Add URL controller and update project dependencies.

---
+++ connect.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+async function connectToMongooDB(url) {
+  return mongoose.connect(url);
+}
+
+module.exports = {
+  connectToMongooDB,
+};
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const { connectToMongooDB } = require("./connect");
 
+const urlRoute = require("./routes/url");
 const app = express();
 const PORT = 8001;
+
+connectToMongooDB("mongodb://127.0.0.1:27017/short-url").then(() =>
+  console.log("MongoDB Connected"),
+);
+
+app.use("/url", urlRoute);
 
 app.listen(PORT, () => {
   console.log(`Server Started at PORT:${PORT}`);
