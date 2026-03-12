ca1e1db88ab77103afd16c1b14ebb5fba5e7c74c
asutosh-das <ashutoshdas788@gmail.com>
2026-03-13 00:12:29 +0530

    Implement URL shortening service with API endpoints for generation, redirection, and visit analytics.

--- controllers/url.js
+++ controllers/url.js
@@ -3,7 +3,7 @@ const URL = require("../models/url");
 
 async function handleGenerateNewShortURL(req, res) {
   const body = req.body;
-  if (!body.url) {
+  if (!body || !body.url) {
     return res.status(400).json({ error: "url is required" });
   }
   const shortID = shortid();
@@ -16,6 +16,19 @@ async function handleGenerateNewShortURL(req, res) {
   return res.json({ id: shortID });
 }
 
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+  
+  if (!result) return res.status(404).json({ error: "URL not found" });
+
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
+}
+
 module.exports = {
   handleGenerateNewShortURL,
+  handleGetAnalytics,
 };
--- index.js
+++ index.js
@@ -4,14 +4,31 @@ const { connectToMongooDB } = require("./connect");
 const urlRoute = require("./routes/url");
 const app = express();
 const PORT = 8001;
+const URL = require("./models/url");
 
 connectToMongooDB("mongodb://127.0.0.1:27017/short-url").then(() =>
   console.log("MongoDB Connected"),
 );
 
-// app.use(express.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: { timeStamp: Date.now() },
+      },
+    },
+  );
+  res.redirect(entry.redirectURL);
+});
 
 app.listen(PORT, () => {
   console.log(`Server Started at PORT:${PORT}`);
